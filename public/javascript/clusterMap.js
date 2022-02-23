aab5a2a89e0feaa275485e0e487213fd5d7dc3a8
Siddhartha <siddharthaarora61@gmail.com>
2022-02-23 21:30:18 +0530

    Fixing map error

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [74.81767380474436, 23.76278209193778],
     zoom: 3
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
